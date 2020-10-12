abcc82dea780d12aed9365fdaeeb2a9b23339170
poojan1812 <54631540+poojan1812@users.noreply.github.com>
2020-10-12 17:56:21 +0530

    QUICK FIXES

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ echo "Hello there";
 
 echo "<br>";
 echo "<br>";
-echo "This is my ip:".$_SERVER['HTTP_HOST'] ;
-
+echo "This is my ip:".$_SERVER['SERVER_ADDR'] ;
+echo "<br>";
+echo "<br>";
+echo "forwarded from:".$_SERVER['HTTP_X_FORWARDED_FOR'];
 ?>
